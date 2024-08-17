000a678140f4ba330680340cae1c28b03b4ead9f
Erie Putranto <erieputranto@gmail.com>
2024-08-17 18:01:49 +0700

    update: hero section | fix font size

--- src/components/custom/HeroSection.tsx
+++ src/components/custom/HeroSection.tsx
@@ -6,24 +6,21 @@ export function TypewriterEffectSmoothDemo() {
   const words = [
     {
       text: "Temukan",
+      className: "text-3xl antialiased",
     },
     {
       text: "Hunian",
+      className: "text-3xl antialiased",
     },
+
     {
-      text: "Idaman Anda",
-    },
-    {
-      text: "bersama",
-    },
-    {
-      text: "Bosqu Properti.",
-      className: "text-blue-500 dark:text-blue-500",
+      text: "Idaman Anda.",
+      className: "text-blue-500 dark:text-blue-500 text-3xl antialiased",
     },
   ];
   return (
-    <div className="flex flex-col items-center justify-center ">
-      <h1>
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="antialiased text-9xl">
         <TypewriterEffectSmooth words={words} />
       </h1>
     </div>
