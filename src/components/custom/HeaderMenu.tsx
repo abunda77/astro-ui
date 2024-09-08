f7754c55bc105205ec094c735a1355a0660cba95
Erie Putranto <erieputranto@gmail.com>
2024-09-08 11:28:30 +0700

    update: nav menu mobile Headermenu2

--- src/components/custom/HeaderMenu.tsx
+++ src/components/custom/HeaderMenu.tsx
@@ -44,20 +44,20 @@ const HeaderMenu: React.FC<HeaderMenuProps> = ({ menuitems }) => {
       </Button>
 
       {isMenuOpen && (
-        <div className="absolute left-0 right-0 z-50 bg-white rounded-b-lg shadow-lg dark:bg-gray-900">
-          <Nav className="w-full text-gray-800 dark:text-gray-200">
+        <div className="absolute left-0 right-0 z-50 text-left bg-white rounded-b-lg shadow-lg dark:bg-gray-900">
+          <Nav className="flex flex-col w-full text-gray-800 dark:text-gray-200">
             {menuitems.map((item, index) =>
               item.items ? (
                 <Nav.Menu
                   key={index}
                   title={item.title}
-                  className="hover:bg-gray-200 dark:hover:bg-gray-800"
+                  className="w-full hover:bg-gray-200 dark:hover:bg-gray-800 dark:text-gray-100"
                 >
                   {item.items.map((subItem, subIndex) => (
                     <Nav.Item
                       key={subIndex}
                       href={subItem.path}
-                      className="hover:bg-gray-100 dark:hover:bg-gray-700"
+                      className="w-full pl-4 hover:bg-gray-100 dark:hover:bg-gray-700 "
                     >
                       {subItem.title}
                     </Nav.Item>
@@ -67,19 +67,17 @@ const HeaderMenu: React.FC<HeaderMenuProps> = ({ menuitems }) => {
                 <Nav.Item
                   key={index}
                   href={item.path}
-                  className="hover:bg-gray-100 dark:hover:bg-gray-700"
+                  className="flex justify-start w-full text-left hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-green-100"
                 >
                   {item.title}
                 </Nav.Item>
               )
             )}
           </Nav>
-          <div className="pt-4 pb-3 border-t border-gray-200 dark:border-gray-700">
-            <div className="flex items-center justify-between px-5 py-2">
-              <div className="flex items-center space-x-3">
-                <AvatarComponent />
-                <LoginButtons />
-              </div>
+          <div className="pt-4 pb-3 border-t border-gray-200 dark:border-gray-700 ">
+            <div className="flex flex-col items-center px-5 py-2 space-y-3">
+              <AvatarComponent />
+              <LoginButtons />
               <ModeToggle />
             </div>
           </div>
